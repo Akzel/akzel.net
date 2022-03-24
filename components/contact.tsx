6c9cc85872b58e2fbd3cc3219d258884cabe4592
Axel <axelm19@ru.is>
2022-03-24 18:14:13 +0000

    think all spacing is fixed now

--- components/contact.tsx
+++ components/contact.tsx
@@ -14,43 +14,43 @@ export default function Contact() {
           <h1 className={styles.title}>
             Get in <a>touch!</a>
           </h1>
-          <div className={styles.grid}>
-            <a>
-              <div className={styles.notify}>Copied!</div>
-              <SocialIcon
-                className={styles.social}
-                network="discord"
-                onClick={() => {
-                  navigator.clipboard.writeText("Akzel#6076");
-                  alertElement.innerHTML = "<h1>Copied!: Akzel#6076</a>";
-                  setTimeout(function () {
-                    alertElement.innerHTML = "";
-                  }, 300);
-                }}
-              ></SocialIcon>
 
-              <SocialIcon
-                className={styles.social}
-                network="email"
-                onClick={() => {
-                  navigator.clipboard.writeText("axel@akzel.xyz");
-                  alertElement.innerHTML = "<h1>Copied!: axel@akzel.xyz</a>";
-                  setTimeout(function () {
-                    alertElement.innerHTML = "";
-                  }, 300);
-                }}
-              ></SocialIcon>
+          <a>
+            <div className={styles.notify}>Copied!</div>
+            <SocialIcon
+              className={styles.social}
+              network="discord"
+              onClick={() => {
+                navigator.clipboard.writeText("Akzel#6076");
+                alertElement.innerHTML = "<h1>Copied!: Akzel#6076</a>";
+                setTimeout(function () {
+                  alertElement.innerHTML = "";
+                }, 300);
+              }}
+            ></SocialIcon>
 
-              <SocialIcon
-                className={styles.social}
-                url="https://www.instagram.com/axel_____________/"
-              ></SocialIcon>
-              <SocialIcon
-                className={styles.social}
-                url="https://www.linkedin.com/in/axel-magn%C3%BAsson-912528227/"
-              ></SocialIcon>
-            </a>
-          </div>{" "}
+            <SocialIcon
+              className={styles.social}
+              network="email"
+              onClick={() => {
+                navigator.clipboard.writeText("axel@akzel.xyz");
+                alertElement.innerHTML = "<h1>Copied!: axel@akzel.xyz</a>";
+                setTimeout(function () {
+                  alertElement.innerHTML = "";
+                }, 300);
+              }}
+            ></SocialIcon>
+
+            <SocialIcon
+              className={styles.social}
+              url="https://www.instagram.com/axel_____________/"
+            ></SocialIcon>
+            <SocialIcon
+              className={styles.social}
+              url="https://www.linkedin.com/in/axel-magn%C3%BAsson-912528227/"
+            ></SocialIcon>
+          </a>
+
           <footer className={styles.footer}>
             <Link scroll={true} href="#Welcome">
               &#9650;
