736fd91ace2feee2d159c780ed69a9dd0c2089ef
Axel <axelm19@ru.is>
2022-03-24 17:51:34 +0000

    maybe?

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,59 +4,63 @@ import { SocialIcon } from "react-social-icons";
 import React from "react";
 
 export default function Contact() {
-  const alertElement = document.getElementById("alert")!;
+  if (typeof window !== "undefined") {
+    const alertElement = document.getElementById("alert")!;
 
-  return (
-    <>
-      {" "}
-      <main className={styles.main} id="Contact">
-        <h1 className={styles.title}>
-          Get in <a>touch!</a>
-        </h1>
-        <div className={styles.grid}>
-          <a>
-            <div className={styles.notify}>Copied!</div>
-            <SocialIcon
-              className={styles.social}
-              network="discord"
-              onClick={() => {
-                navigator.clipboard.writeText("Akzel#6076");
-                alertElement.innerHTML = "<h1>Copied!: Akzel#6076</a>";
-                setTimeout(function () {
-                  alertElement.innerHTML = "";
-                }, 300);
-              }}
-            ></SocialIcon>
+    return (
+      <>
+        {" "}
+        <main className={styles.main} id="Contact">
+          <h1 className={styles.title}>
+            Get in <a>touch!</a>
+          </h1>
+          <div className={styles.grid}>
+            <a>
+              <div className={styles.notify}>Copied!</div>
+              <SocialIcon
+                className={styles.social}
+                network="discord"
+                onClick={() => {
+                  navigator.clipboard.writeText("Akzel#6076");
+                  alertElement.innerHTML = "<h1>Copied!: Akzel#6076</a>";
+                  setTimeout(function () {
+                    alertElement.innerHTML = "";
+                  }, 300);
+                }}
+              ></SocialIcon>
 
-            <SocialIcon
-              className={styles.social}
-              network="email"
-              onClick={() => {
-                navigator.clipboard.writeText("axel@akzel.xyz");
-                alertElement.innerHTML = "<h1>Copied!: axel@akzel.xyz</a>";
-                setTimeout(function () {
-                  alertElement.innerHTML = "";
-                }, 300);
-              }}
-            ></SocialIcon>
+              <SocialIcon
+                className={styles.social}
+                network="email"
+                onClick={() => {
+                  navigator.clipboard.writeText("axel@akzel.xyz");
+                  alertElement.innerHTML = "<h1>Copied!: axel@akzel.xyz</a>";
+                  setTimeout(function () {
+                    alertElement.innerHTML = "";
+                  }, 300);
+                }}
+              ></SocialIcon>
 
-            <SocialIcon
-              className={styles.social}
-              url="https://www.instagram.com/axel_____________/"
-            ></SocialIcon>
-            <SocialIcon
-              className={styles.social}
-              url="https://www.linkedin.com/in/axel-magn%C3%BAsson-912528227/"
-            ></SocialIcon>
-          </a>
-        </div>{" "}
-        <footer className={styles.footer}>
-          <Link scroll={true} href="#Welcome">
-            &#9650;
-          </Link>
-        </footer>
-        <span id="alert" className={styles.orange}></span>
-      </main>
-    </>
-  );
+              <SocialIcon
+                className={styles.social}
+                url="https://www.instagram.com/axel_____________/"
+              ></SocialIcon>
+              <SocialIcon
+                className={styles.social}
+                url="https://www.linkedin.com/in/axel-magn%C3%BAsson-912528227/"
+              ></SocialIcon>
+            </a>
+          </div>{" "}
+          <footer className={styles.footer}>
+            <Link scroll={true} href="#Welcome">
+              &#9650;
+            </Link>
+          </footer>
+          <span id="alert" className={styles.orange}></span>
+        </main>
+      </>
+    );
+  } else {
+    return <h1>oopsie...</h1>;
+  }
 }
