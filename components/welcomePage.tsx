66cd101c01f35ad65cca1f92ea5bbd7ef66f512b
Axel <axelm19@ru.is>
2022-03-24 18:06:03 +0000

    fex

--- components/welcomePage.tsx
+++ components/welcomePage.tsx
@@ -8,15 +8,15 @@ export default function Welcome() {
         <h1 className={styles.title}>
           Welcome to <a>Akzel.xyz</a>
         </h1>
-        <div className={styles.grid}>
-          <a className={styles.card}>
-            <h2>Looks pretty empty here.. </h2>
-            <p>
-              I plan on adding more stuff, for now this page serves to show off
-              the scrolling feature
-            </p>
-          </a>
-        </div>{" "}
+
+        <a className={styles.card}>
+          <h2>Looks pretty empty here.. </h2>
+          <p>
+            I plan on adding more stuff, for now this page serves to show off
+            the scrolling feature
+          </p>
+        </a>
+
         <footer className={styles.footer}>
           <Link scroll={true} href="#age">
             &#9650;
