d6aa7ff5623d57669836be7eac59da8b76a36308
Axel Magnússon <axel@akzel.net>
2024-11-03 21:35:15 +0100

    Update gallery.tsx

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
-const PIC_COUNT = 16;
+const PIC_COUNT = 30;
 
 export default function Gallery() {
   const [current, setCurrent] = useState(0);
